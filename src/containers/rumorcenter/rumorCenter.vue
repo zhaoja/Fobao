a5fbfd3a5776f962282e307328dd93caaa5f33d8
zhaojia <332487429@qq.com>
2018-12-02 11:55:52 +0800

    new

--- src/containers/rumorcenter/rumorCenter.vue
+++ src/containers/rumorcenter/rumorCenter.vue
@@ -10,7 +10,10 @@
 				</div>
 			</div>
 
-			<div class="page-tab-container" @touchstart='touchStart' @touchmove='touchMove' @touchend='touchEnd'>
+			<div class="page-tab-container" 
+			@touchstart='touchStart' 
+			@touchmove='touchMove'
+			@touchend='touchEnd'>
 				<mt-tab-container class="page-tabbar-tab-container" v-model="active"  swipeable>
 					<mt-tab-container-item :id="'tab-container'+list1.type">
 
@@ -107,7 +110,8 @@
 				active: 'tab-container1',
 				topName: "辟谣中心",
 				allLoaded: false,
- 
+				loadTop:false,
+				
 				isShow: false,
 				startX: 0, //开始触摸的位置
 				moveX: 0, //滑动时的位置
@@ -122,62 +126,67 @@
 
 			var navBtn = document.getElementsByClassName("mint-button")[0];
 			navBtn.classList.add("navAction")
+			
+			document.getElementsByClassName("mint-loadmore-content")
 
 		},
 		methods: {
 			getType(a) {
 
 			},
-			loadTop() {
-
-				this.$refs.loadmore.onTopLoaded();
-				console.log(this.$refs.loadmore)
-			},
+// 			loadTop() {
+// 				if(false) {
+// 					this.$refs.loadmore.onTopLoaded();
+// 				}
+// 				console.log(this.$refs.loadmore)
+// 				return false
+// 				
+// 			},
 			loadBottom() {},
 			touchStart: function(ev) {
-				//console.log(1)
+// 				//console.log(1)
 				ev = ev || event;
-				ev.preventDefault();
-				//                      console.log(ev.targetTouches);
-				//                      console.log(ev.changedTouches);
+				// ev.preventDefault();
+// 				//                      console.log(ev.targetTouches);
+// 				//                      console.log(ev.changedTouches);
 				if (ev.touches.length == 1) { //tounches类数组，等于1时表示此时有只有一只手指在触摸屏幕
 					this.startX = ev.touches[0].clientX; // 记录开始位置
 				//	console.log(this.startX)
 				}
 			},
 			touchMove: function(ev) {
-				//console.log(2)
+				console.log(2)
 				ev = ev || event;
 				ev.preventDefault();
-// 				let btnWidth = this.$refs.remove.offsetWidth; //$refs 减少获取dom节点的消耗
-// 				let btnImg = this.$refs.btnImg.offsetWidth;
-// 				console.log(ev.targetTouches);
-// 				console.log(ev.changedTouches);
-				if (ev.touches.length == 1) {
-					//滑动时距离浏览器左侧的距离
-					this.moveX = ev.touches[0].clientX;
-					
-					//实时的滑动的距离-起始位置=实时移动的位置
-// 					this.disX = this.moveX - this.startX;
-// 					if (this.disX < 0 || this.disX == 0) {
-// 						this.slideEffect = 'transform:translateX(0px)';
-// 					} else if (this.disX > 0) {
-// 						this.slideEffect = 'transform:translateX(' + this.disX + 'px)';
-// 
-// 						// 最大也只能等于删除按钮宽度 
-// 						if (this.disX >= btnWidth) {
-// 							this.slideEffect = 'transform:translateX(' + (btnWidth - btnImg) + 'px)';
-// 						}
-// 					}
-				}
+// // 				let btnWidth = this.$refs.remove.offsetWidth; //$refs 减少获取dom节点的消耗
+// // 				let btnImg = this.$refs.btnImg.offsetWidth;
+// // 				console.log(ev.targetTouches);
+// // 				console.log(ev.changedTouches);
+// 				if (ev.touches.length == 1) {
+// 					//滑动时距离浏览器左侧的距离
+// 					this.moveX = ev.touches[0].clientX;
+// 					
+// 					//实时的滑动的距离-起始位置=实时移动的位置
+// // 					this.disX = this.moveX - this.startX;
+// // 					if (this.disX < 0 || this.disX == 0) {
+// // 						this.slideEffect = 'transform:translateX(0px)';
+// // 					} else if (this.disX > 0) {
+// // 						this.slideEffect = 'transform:translateX(' + this.disX + 'px)';
+// // 
+// // 						// 最大也只能等于删除按钮宽度 
+// // 						if (this.disX >= btnWidth) {
+// // 							this.slideEffect = 'transform:translateX(' + (btnWidth - btnImg) + 'px)';
+// // 						}
+// // 					}
+// 				}
 			},
 			touchEnd: function(ev) {
-				//console.log(3)
+// 				//console.log(3)
 				ev = ev || event;
 				ev.preventDefault();
-// 				let btnWidth = this.$refs.remove.offsetWidth;
-// 				let btnImg = this.$refs.btnImg.offsetWidth;
-				//                      console.log(ev.changedTouches);
+// // 				let btnWidth = this.$refs.remove.offsetWidth;
+// // 				let btnImg = this.$refs.btnImg.offsetWidth;
+// 				//                      console.log(ev.changedTouches);
 				if (ev.changedTouches.length == 1) {
 					let endX = ev.changedTouches[0].clientX;
 					this.disX = endX - this.startX;
